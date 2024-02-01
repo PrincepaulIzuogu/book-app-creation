0f209f74b38920c2f3a661d79dd666261a89ee22
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2024-02-01 11:26:02 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+function validateForm() {
+    var title = document.getElementById('title').value;
+    var author = document.getElementById('author').value;
+    var publicationYear = document.getElementById('publication_year').value;
+
+    if (title === '' || author === '' || publicationYear === '') {
+        alert('All fields must be filled out');
+        return false;
+    }
+
+    return true;
+}
